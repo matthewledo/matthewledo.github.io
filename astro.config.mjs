64c5c89e52469f557bb86452df86557420ce634b
Matthew Ledo <matthewf.ledo@gmail.com>
2026-02-08 15:24:08 -0700

    added site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://matthewledo.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
